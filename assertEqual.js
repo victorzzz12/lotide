d16f383964df9a49ccbaa892fbdb0a431dbe46a7
Victor Liang <victor.zw.liang@gmail.com>
2020-05-27 02:05:01 +0000

    Lint changes

--- assertEqual.js
+++ assertEqual.js
@@ -3,7 +3,7 @@ const assertEqual = function(actual, expected) {
     console.log(`Assertion Passed: ${actual} === ${expected}`);
   } else {
     console.log(`Assertion Failed: ${actual} !== ${expected}`);
-  }   
+  }
 };
 
 // TEST CODE
