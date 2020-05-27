728e40c4df82d382431781e3f7fe4a87cca55a9c
Victor Liang <victor.zw.liang@gmail.com>
2020-05-27 01:59:35 +0000

    Added equal arrays and assertEquals changes

---
+++ eqArrays.js
@@ -0,0 +1,37 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+
+const eqArrays = function(array1, array2) {
+  if (array1.length !== array2.length) {
+    return false;
+  }
+  else if (array1 === array2) {
+    return true;
+  }
+  else if (array1 == null || array2 == null) {
+    return false;
+  }
+  for (let i = 0; i < array1.length; i++) {
+    if (array1[i] !== array2[i]) return false;
+  }
+  return true;
+};
+
+
+
+
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true);
+
+
+
+eqArrays([1, 2, 3], [1, 2, 3]); // => true
+eqArrays([1, 2, 3], [3, 2, 1]); // => false
+
+eqArrays(["1", "2", "3"], ["1", "2", "3"]); // => true
+eqArrays(["1", "2", "3"], ["1", "2", 3]); // => false
