8c1cc3ec957dda3fd120e5ee3298cb9bbed8b868
Victor Liang <victor.zw.liang@gmail.com>
2020-05-27 20:39:13 +0000

    Added findKeyByValue.js

---
+++ findKeyByValue.js
@@ -0,0 +1,27 @@
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`Assertion Passed: ${actual} === ${expected}`);
+  } else {
+    console.log(`Assertion Failed: ${actual} !== ${expected}`);
+  }
+};
+
+const bestTVShowsByGenre = { 
+  sci_fi: "The Expanse",
+  comedy: "Brooklyn Nine-Nine",
+  drama:  "The Wire"
+};
+
+const findKeyByValue = function(object, showName) {
+  let keyArray = Object.keys(object);
+  for (let i = 0; i < keyArray.length; i++) {
+    if (object[keyArray[i]] === showName) {
+      return keyArray[i];
+    }
+  }
+}
+
+console.log(findKeyByValue(bestTVShowsByGenre, "The Wire"));
+
+assertEqual(findKeyByValue(bestTVShowsByGenre, "The Wire"), "drama");
+assertEqual(findKeyByValue(bestTVShowsByGenre, "That '70s Show"), undefined);
